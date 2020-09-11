12b6c17171e2a57bda8cb429414a5f56e327d66d
aadityachaprana <aadityachaprana01@gmail.com>
2020-09-11 22:38:01 +0530

    Updates

---
+++ precache-manifest.ae8e94b1a0ac434431dda92ccbe78c6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91479382cf6b21c21bb3d83fc3b770c8",
+    "url": "/newsapp/index.html"
+  },
+  {
+    "revision": "8bd944cc4d709417d39e",
+    "url": "/newsapp/static/css/main.cb24f8ec.chunk.css"
+  },
+  {
+    "revision": "08bb126ec7c04d228bd2",
+    "url": "/newsapp/static/js/2.db8506f4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/newsapp/static/js/2.db8506f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd944cc4d709417d39e",
+    "url": "/newsapp/static/js/main.6d5996bd.chunk.js"
+  },
+  {
+    "revision": "14c2ef09e13035347734",
+    "url": "/newsapp/static/js/runtime-main.eff0c8ba.js"
+  }
+]);
